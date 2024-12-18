482305979a664b8fc95a6adf9ca17aeb06933a25
b5h22 <byoo3@myseneca.ca>
2024-12-17 22:29:47 -0500

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://b5h22.github.io',
+  base: '/tech-blog',
+  integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark',
+      wrap: true,
+    },
+  },
+});
